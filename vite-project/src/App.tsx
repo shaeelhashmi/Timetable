70e9c3fbc2db16fbfccd0cd204478a5a86ecd869
shaeelhashmi <shaeelh001@gmail.com>
2024-09-22 07:28:40 -0700

    added faculty page

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Signup from './Components/Signup'
 import Navbar from './Components/Navbar'
+import Faculty from './Components/Faculty'
 function App() {
   return (
     <>
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route  path="/login" element={<Login />}  />
         <Route path='/signup' element={<Signup></Signup>}></Route>
+        <Route path='/data/faculty' element={<Faculty></Faculty>}></Route>
       </Routes>
     </Router>
     </>
--- vite-project/src/Components/Faculty.tsx
+++ vite-project/src/Components/Faculty.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
-
+import Input from "./Input";
+import { useState } from "react";
 export default function Faculty() {
+    const [error, setError] = useState(false);
+    const [error2, setError2] = useState(false);
   return (
-    <div>
-      
+    <div className="mt-24">
+      <h1 className="text-4xl text-center">Add Faculty</h1>
+      <form action="#" className="flex flex-col items-center justify-center mt-12">
+        <Input label="First name:" type="text" placeholder="Enter Name" name="firstName"/>
+        <Input label="Last name:" type="text" placeholder="Enter Name" name="lastName"/>
+        <Input label="ID:" type="text" placeholder="Enter unique ID" name="ID"/>
+       <p className="h-5 text-red-900">{error?"Member already exists":""}</p>
+        <button className="px-4 py-2 mt-4 font-bold text-white bg-blue-500 rounded hover:bg-blue-700">Submit</button>
+      </form>
+        <h1 className="mt-12 text-4xl text-center">Remove faculty</h1>
+      <form action="#" className="flex flex-col items-center justify-center mt-12">
+          <Input label="ID:" type="text" placeholder="Enter unique ID" name="ID"/>
+          <p className="h-5 text-red-900">{error2?"Member does not exist":""}</p>
+        <button className="px-4 py-2 mt-4 font-bold text-white bg-blue-500 rounded hover:bg-blue-700">Submit</button>
+      </form>
     </div>
   )
 }
